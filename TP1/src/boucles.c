a7b6d9801fae0c59dc65e8ff9af57dc78bf3ed96
Thorakas <102747537+Thorakas@users.noreply.github.com>
2025-11-21 11:24:19 +0100

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -3,6 +3,7 @@
 int main() {
     int compteur = 5;
 
+    if (compteur <= 10 {
     for (int i = 1; i <= compteur; i++) {
         for (int j = 1; j <= i; j++) {
 
@@ -16,6 +17,11 @@ int main() {
         }
         printf("\n");
     }
+    }
 
+    else {
+        printf("merci de saisir une valeur inférieur ou égal à 10")
+            }
     return 0;
 }
+
