a1535bffae8f4764bb87f2b7589030e3f88ab641
Thorakas <102747537+Thorakas@users.noreply.github.com>
2025-11-21 11:19:58 +0100

    Update boucles.c

---
+++ TP1/src/boucles.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int compteur = 5;
+
+    for (int i = 1; i <= compteur; i++) {
+        for (int j = 1; j <= i; j++) {
+
+            // Début et fin en '*'
+            if (j == 1 || j == i) {
+                printf("* ");
+            }
+            else {
+                printf("# ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
